2829b293e5b80453bf8fc4df28c540eec9e64323
swapper <ncamacho97@gmail.com>
2017-05-25 19:45:03 -0500

    work

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,25 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list-rentals');
+
+test('should link to information about the company.', function (assert) {
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+test('should link to contact information', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function() {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+test('should list available rentals.', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
---
+++ tests/integration/components/rental-listing-test.js
@@ -0,0 +1,15 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
+  integration: true
+});
+
+test('should display rental details', function(assert) {
+
+});
+
+test('should toggle wide class on click', function(assert) {
+
+});
